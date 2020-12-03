ec0aa9ca09f6ce30231bced2e75b855bbb4c6ec5
elizabethstevenson <60235792+elizabethstevenson@users.noreply.github.com>
2020-12-03 10:56:56 -0800

    commented code :)

--- proj4.cpp
+++ proj4.cpp
@@ -14,6 +14,7 @@ using std::string;
 using std::vector;
 using std::min;
 
+//variables to hold all the palindromes and input
 string input;
 string caseOnePal;
 string caseTwoPal;
@@ -36,7 +37,8 @@ void caseOne(string input) {
     }
     //input is palindrome
     caseOnePal = input;
-    /*
+
+    /* previous palindrome code just incase new stuff didn't work
     bool isPal = false;
     if(input.length() == 1) {
         isPal = true;
@@ -77,6 +79,7 @@ void caseTwo(string input) {
     string string1 = input;
     string string2 = input;
     reverse(string2.begin(), string2.end());
+    //splits the string so then there's a pivot and the middle letter is not repeated
     string2 = input + "^" + string2;
     vector<int> temp(string2.size());
     unsigned int i = 1;
@@ -108,6 +111,7 @@ void caseThree(string input) {
     string string1 = input;
     string string2 = input;
     reverse(string2.begin(), string2.end());
+    //splits the string so then there's a pivot and the middle letter is not repeated
     string2 = input + "^" + string2;
     vector<int> temp(string2.size());
     unsigned int i = 1;
@@ -141,39 +145,45 @@ void caseFour(string input) {
 }
 */
 
+
+//the actual dynamic programming part
 //formula in box
 // p(i + 1, j − 1) + 2  if S[i] = S[j]
 // min {p(i + 1, j) + 2, p(i, j − 1) + 2}  if S[i] =\= S[j]
 int boxFormTwo(int i, int j) {
+    int num = 0;
     //i = 0;
     //j = input.length()-1;
     //string palSection = caseTwoPal.find(input);
     //reverse(palSection.begin(), palSection.end());
     if(caseTwoPal.at(i) == caseTwoPal.at(j)) {
-        //return boxFormTwo(i+1,j-1)+2;
-        return caseTwoPal.length();
+        num = boxFormTwo(i+1,j-1)+2;
+        //return caseTwoPal.length();
     } else if (caseTwoPal.at(i) != caseTwoPal.at(j)) {
-        //return min(boxFormTwo(i+1,j)+2, boxFormTwo(i,j-1)+2);
-        return caseTwoPal.length();
+        num = min(boxFormTwo(i+1,j)+2, boxFormTwo(i,j-1)+2);
+        //return caseTwoPal.length();
     }
+    return num;
 }
 
 //formula in box
 // p(i + 1, j − 1) + 2  if S[i] = S[j]
 // min {p(i + 1, j) + 2, p(i, j − 1) + 2}  if S[i] =\= S[j]
 int boxFormThree(int i, int j) {
+    int num = 0;
     //i = 0;
     //j = input.length()-1;
     //string palSection = caseThreePal.find(input);
     //reverse(palSection.begin(), palSection.end());
     //if(caseThreePal.at(i).compare(caseThreePal.at(j)) == 0) {
     if(caseThreePal.at(i) == caseThreePal.at(j)) {
-        //return boxFormThree(i+1,j-1)+2;
-        return caseThreePal.length();
+        num = boxFormThree(i+1,j-1)+2;
+        //return caseThreePal.length();
     } else if (caseThreePal.at(i) != caseThreePal.at(j)) {
-        //return min(boxFormThree(i+1,j)+2, boxFormThree(i,j-1)+2);
-        return caseThreePal.length();
+        num = min(boxFormThree(i+1,j)+2, boxFormThree(i,j-1)+2);
+        //return caseThreePal.length();
     }
+    return num;
 }
 
 void palindrome() {
